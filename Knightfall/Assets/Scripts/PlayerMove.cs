1d90f600accf23877f91c88526602de28828f341
Breeland Abrams <breeland.abrams@gmail.com>
2018-04-15 20:40:52 -0700

    Testing Level Creation

---
+++ Knightfall/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float moveSpeed = 200f;
+    private Rigidbody2D body2D;
+    private PlayerState playerState;
+
+    void Awake()
+    {
+        body2D = GetComponent<Rigidbody2D>();
+        playerState = GetComponent<PlayerState>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (playerState.actionButton = Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            body2D.velocity = new Vector2(moveSpeed, 0);
+        }
+        if (playerState.actionButton = Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            body2D.velocity = new Vector2(-moveSpeed, 0);
+        }
+
+    }
+}
